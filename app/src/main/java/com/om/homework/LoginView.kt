f5ec227d06a1e7771eb8a0e81d37b061c8b3fb33
Oleksand Matviienko <qniwwwersss@gmail.com>
2024-12-26 09:45:08 +0100

    feat: Working on login page; added bg, sample layout, and rounded box for input

---
+++ app/src/main/java/com/om/homework/LoginView.kt
@@ -0,0 +1,16 @@
+package com.om.homework
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class LoginView : FrameLayout {
+
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttrs: Int) : super(
+        context, attrs, defStyleAttrs) {}
+
+}
