a2e662752e06747bde52912fb788a55f6660c785
Oleksand Matviienko <qniwwwersss@gmail.com>
2024-12-26 09:59:30 +0100

    feat: Implemented basic functionality of displaying inputted email/password

--- app/src/main/java/com/om/homework/LoginView.kt
+++ app/src/main/java/com/om/homework/LoginView.kt
@@ -2,15 +2,47 @@ package com.om.homework
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
 import android.widget.FrameLayout
+import android.widget.Toast
 
 class LoginView : FrameLayout {
 
-    constructor(context: Context) : super(context) {}
+    private lateinit var emailText: EditText
+    private lateinit var passwordText: EditText
+    private lateinit var loginButton: Button
 
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(context)
+    }
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttrs: Int) : super(
-        context, attrs, defStyleAttrs) {}
+        context, attrs, defStyleAttrs) {
+        init(context)
+    }
 
+    private fun init(context: Context) {
+        LayoutInflater.from(context).inflate(R.layout.login_layout, this, true)
+
+        emailText = findViewById(R.id.email_input)
+        passwordText = findViewById(R.id.password_input)
+        loginButton = findViewById(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            val email = emailText.text.toString()
+            val password = passwordText.text.toString()
+
+            if (email.isNotBlank() && password.isNotBlank()) {
+                Toast.makeText(context, "Email: $email, Password: $password", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(context, "Please enter both email and password", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
