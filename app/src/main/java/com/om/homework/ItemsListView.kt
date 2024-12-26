1a0faefaec7d97af06785ad8ff9719fa94097e1b
Oleksand Matviienko <qniwwwersss@gmail.com>
2024-12-26 11:43:50 +0100

    feat: Added fruitList and fruitImages to item ListView

--- app/src/main/java/com/om/homework/ItemsListView.kt
+++ app/src/main/java/com/om/homework/ItemsListView.kt
@@ -1,4 +1,10 @@
 package com.om.homework
 
 class ItemsListView {
+
+    var fruitList = {"Apple"; "Banana"; "Orange"; "Lemon"}
+    var fruitsImages = {R.drawable.apple; R.drawable.banana; R.drawable.orange; R.drawable.lemon}
+
+
+
 }
