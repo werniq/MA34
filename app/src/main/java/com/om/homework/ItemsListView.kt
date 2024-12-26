b260bbda253e399d2c2fbf2e0c81869d7e63825d
Oleksand Matviienko <qniwwwersss@gmail.com>
2024-12-26 12:21:14 +0100

    feat: Implemented itemsListView, added onCreate func and integrated customAdapter

--- app/src/main/java/com/om/homework/ItemsListView.kt
+++ app/src/main/java/com/om/homework/ItemsListView.kt
@@ -1,10 +1,22 @@
 package com.om.homework
 
-class ItemsListView {
+import android.os.Bundle
+import android.content.Context
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 
-    var fruitList = {"Apple"; "Banana"; "Orange"; "Lemon"}
-    var fruitsImages = {R.drawable.apple; R.drawable.banana; R.drawable.orange; R.drawable.lemon}
+class ItemsListView : AppCompatActivity() {
 
+    private val fruitList = arrayOf("Apple", "Banana", "Orange", "Lemon")
+    private val fruitsImages = intArrayOf(R.drawable.apple, R.drawable.banana, R.drawable.orange, R.drawable.lemon)
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_items)
 
+        val mListView = findViewById<ListView>(R.id.customListView)
+        val adapter = CustomAdapter(this, fruitList, fruitsImages)
+
+        mListView.adapter = adapter
+    }
 }
