2ab757c7b1449da7084f6e49051ac2edf82ea8b6
Oleksand Matviienko <qniwwwersss@gmail.com>
2024-12-26 10:01:19 +0100

    feat: Added firebase authentication dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-auth")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
